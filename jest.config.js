782683efc3016868f4c25341da6f5c5b5cb4b616
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-01-21 18:45:40 -0500

    reorg

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   projects: [
-    '<rootDir>/libs/a-react-lib',
-    '<rootDir>/libs/a-angular-feature-lib',
-    '<rootDir>/libs/a-angular-util-lib',
     '<rootDir>/apps/angular-nest-app/ui',
     '<rootDir>/apps/angular-nest-app/backend',
     '<rootDir>/apps/next-nest-app/backend',
     '<rootDir>/apps/next-nest-app/ui',
+    '<rootDir>/libs/a-workspace-lib',
+    '<rootDir>/libs/a-react-lib',
+    '<rootDir>/libs/a-angular-feature-lib',
+    '<rootDir>/libs/a-angular-util-lib',
   ],
 };
